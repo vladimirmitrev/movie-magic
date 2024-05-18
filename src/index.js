de60aa4e0d1f0fb58d60d7d5f3816b987e6837df
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-18 10:45:38 +0300

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('init');
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>');
+});
+
+app.listen(port , () => console.log(`Server is listening on port ${port}...`));
