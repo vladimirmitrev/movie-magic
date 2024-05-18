b9e12bddf159e7fe4da5950f0896b9a6802aa9f6
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-18 15:11:14 +0300

    Show dynamic movies on search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,9 +5,16 @@ router.get('/', (req, res) => {
     const movies = movieService.getAll();
     res.render('home', {movies} );
 });
+
 router.get('/about', (req, res) => {
     res.render('about');
 });
+
+router.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+    res.render('search', { movies });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
