7b6b813b4558ffcb79d2abcf5e913fd7d3a5962c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-18 13:23:30 +0300

    Create movie action and service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
+const movieService = require('../services/movieService');
+
 router.get('/create', (req, res) => {
     res.render('create');
 });
+router.post('/create', (req, res) => {
+    // console.log(req.body);
+    const newMovie = req.body;
+    movieService.create(newMovie);
+    res.send('Movies should be created');
+});
+
 
 module.exports = router;
