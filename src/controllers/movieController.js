30f6dc4c594b63a1241062353ca014717073eb1a
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-18 14:47:27 +0300

    Add details rating stars(first method)

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -19,6 +19,8 @@ router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId);
 
+    movie.ratingStars = '&#x2605;'.repeat(movie.rating);
+
     res.render('details', { movie });
 });
 
