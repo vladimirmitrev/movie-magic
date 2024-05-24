81026438ec053243e576406326e8a6df98fa241d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-24 12:51:51 +0300

    Show dropdown with casts

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const movieService = require('../services/movieService');
+const castService = require('../services/castService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -31,8 +32,10 @@ router.get('/movies/:movieId', async (req, res) => {
 
 router.get('/movies/:movieId/attach', async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
+    const casts = await castService.getAll().lean();
 
-    res.render('movie/attach', {...movie});
+    //TODO: remove already added casts
+    res.render('movie/attach', {...movie, casts});
 });
 
 module.exports = router;
