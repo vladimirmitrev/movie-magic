c3d9c08023b244168dbad08afc1d07e3f5b09f3e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-05-31 11:39:49 +0300

    Add cookie parser to express

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const configExpress = (app) => {
     app.use(express.static(path.resolve('src/public')));
     app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    
     return app;
 };
 
