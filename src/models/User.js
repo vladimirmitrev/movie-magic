256cc57727a465ad316991a797489232d4d08157
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-02 13:57:58 +0300

    Fix email regex

--- src/models/User.js
+++ src/models/User.js
@@ -5,17 +5,17 @@ const { match } = require('minimatch');
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required!'],
         lowercase: true,
         unique: true,
-        match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+&/, 'Invalid Email Address'],
+        match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, 'Invalid Email Address'],
         minLength: [10, 'Email should be at least 10 characters long']
     },
     password: {
         type: String,
         match: [/^[a-zA-Z0-9]+/, 'Password should be alphanumeric'],
         minLength: [6, 'Password should be at least 6 characters'],
-        required: true,
+        required: [true, 'Password is required!'],
     }
 });
 
